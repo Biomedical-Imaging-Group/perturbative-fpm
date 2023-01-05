d96a00220b5cbbdff0be479097baaa4f20dfa263
KS-Shen <lucshen.kc@gmail.com>
2023-01-05 23:44:30 +0100

    Local GD feature

--- pyphaseretrieve/algos.py
+++ pyphaseretrieve/algos.py
@@ -166,6 +166,7 @@ class PerturbativePhase:
             for _ in range(linear_n_iter):
                 grad = (-2 * perturbative_model.applyT(y - y_est - perturbative_model.apply(epsilon)))                   
                 epsilon = epsilon - lr*grad
+
             x_est += epsilon
         return x_est
 
@@ -195,6 +196,7 @@ class PerturbativePhase:
                 descent_direction = -r_new + ((r_new.ravel().T.conj()@r_new.ravel())/(res.ravel().T.conj()@res.ravel()))*descent_direction
                 res = r_new
             epsilon = epsilon_expand[:self.pr_model.in_shape[0]] + epsilon_expand[self.pr_model.in_shape[0]:]* 1j
+
             x_est += epsilon
         return x_est
 
