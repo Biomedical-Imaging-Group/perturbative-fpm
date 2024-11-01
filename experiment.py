3e308c28b6bc3977084b3193699dab0bc87f868f
Martin Zach <martin.zach@epfl.ch>
2024-11-01 10:34:24 +0100

    experiment: added parametrized experiments for different patterns

--- experiment.py
+++ experiment.py
@@ -21,17 +21,15 @@ else:
 
 
 # TODO refactor such that same code is called from experiment and simulation
-def PPR(y, microscope, indices, n_iter=100, linear_n_iter=100, alpha=1e5, lamda=0):
+def PPR(y, microscope, indices, n_iter=4, linear_n_iter=100, alpha=1e5, exact_phase=True):
     size = max(microscope.reconstruction_size(i) for i in indices)
     shape = (size, size)
-    model = phaseretrieval.MultiplexedFourierPtychographyPhaseShift(
-        microscope, indices, shape
+    shape = (220, 220)  # TODO this is hardcoded for the experiments ATM, REMOVE!!
+    # TODO the results with inexact phase look better; not sure whats going on
+    model = phaseretrieval.MultiplexedFourierPtychography(
+        microscope, indices, shape, exact_phase=exact_phase
     )
 
-    # TODO we need to discuss how to properly separate all of this stuff
-    # probably have a `residual` function in the model that takes the data as
-    # argument. i dont think it makes sense to give the data into the
-    # microscope class
     def f(x):
         return y - model.forward(x)
 
@@ -40,18 +38,12 @@ def PPR(y, microscope, indices, n_iter=100, linear_n_iter=100, alpha=1e5, lamda=
     x0 = th.ones((1, 1, *shape), dtype=dtypec, device=device)
     x0 *= (th.mean(y[0, 0]) / model.forward(x0)[0, 0].mean()).sqrt()
 
-    def cb(x):
-        print((f(x) ** 2).sum())
-        plt.figure()
-        plt.imshow(th.angle(x)[0, 0].cpu().numpy(), cmap='gray')
-        plt.show()
-
     def solve(A, b):
         return algos.conjugate_gradient(
             A + alpha * pl.Id(), b, x0, n_iter=linear_n_iter, dim=(1, 2, 3)
         )
 
-    return algos.gauss_newton(f=f, x0=x0, n_iter=n_iter, solve=solve, callback=cb)
+    return algos.gauss_newton(f=f, x0=x0, n_iter=n_iter, solve=solve)
 
 
 positions = th.from_numpy(np.load('./dome-positions.npy')
@@ -63,7 +55,7 @@ indices = []
 with open('./led_indices.txt', 'r') as f:
     while (line := f.readline()) != "":
         indices.append(th.from_numpy(
-            np.fromstring(line, sep=',', dtype=np.int32)))
+            np.fromstring(line, sep=',', dtype=np.int64)))
 
 base_path = Path(os.environ['DATASETS_ROOT'])
 experiment = 'attempt_1'
@@ -79,36 +71,69 @@ exposure_df = 200
 path = base_path / 'ptycho' / experiment / \
     f'{phantom}_phase_{exposure_df}ms_{magnification}x_{na:.2f}NA_20240216'
 images_df = imageio.imread(path / 'custom_pat.tif').astype(np.float64)
-print(images_bf.dtype)
 
-images = np.concat((images_bf[:5], images_df[5:] / exposure_df * exposure_bf))[None]
-print(images.shape)
+images = np.concat(
+    (images_bf[:5], images_df[5:] / exposure_df * exposure_bf))[None]
 h, w = images.shape[2:]
-camera_size = 256
-ioff = -100
-joff = -60
+camera_size = 128
+ioff, joff = -90, -10
 istart = (h - camera_size) // 2 + ioff
 jstart = (w - camera_size) // 2 + joff
 images = images[:, :, istart:istart + camera_size, jstart:jstart + camera_size]
 images = th.from_numpy(images).to(dtype).to(device)
+
 clip = False
 if clip:
     for i, im in enumerate(images):
         p = th.quantile(im, 0.99)
         images[i, im > p] = p
-images = images[:, 0:]
-indices = indices[0:]
-print(sum(len(idcs) for idcs in indices))
 
 
 lamda = 0.525
 pixel_size = 6.5
 microscope = Microscope(positions, camera_size, lamda,
                         na, magnification, pixel_size)
-x_est = PPR(images, microscope, indices)
 
-plt.figure()
-plt.imshow(np.fliplr(th.angle(x_est).cpu().numpy()[0, 0]), cmap="gray")
-plt.figure()
-plt.imshow(th.angle(x_est).cpu().numpy()[0, 0], cmap="gray")
-plt.show()
+experiments = {
+    'DPC': {
+        'patterns': [1, 2],  # TODO use 1&2 or 3&4?
+        'n_iter': 1,
+        'linear_n_iter': 200,
+    },
+    'BF-PPR': {
+        'patterns': [0, 1, 2],
+        'n_iter': 4,
+        'linear_n_iter': 100,
+    },
+    'DF-PPR-3': {
+        'patterns': [0, 1, 2, 6, 7, 8],  # TODO ask jonathan if 5 or 6
+        'n_iter': 4,
+        'linear_n_iter': 100,
+    },
+    'DF-PPR-2': {
+        'patterns': [0, 1, 2, 9, 10],
+        'n_iter': 4,
+        'linear_n_iter': 100,
+    },
+}
+
+crop = 8
+
+output_root = Path(os.environ['EXPERIMENTS_ROOT']) / 'phaseretrieval'
+for exact_phase in [True, False]:
+    phase_path = 'exact-phase' if exact_phase else 'inexact-phase'
+    for name, params in experiments.items():
+        our_images = images[:, params['patterns']]
+        our_indices = [indices[pattern] for pattern in params['patterns']]
+        x_est = PPR(our_images, microscope, our_indices,
+                    params['n_iter'], params['linear_n_iter'], exact_phase=exact_phase)
+        x_est = th.angle(x_est[0, 0, crop:-crop, crop:-crop])
+        x_est -= x_est.min()
+        x_est /= x_est.max()
+        x_est *= 255.
+        x_est = x_est.cpu().numpy().astype(np.uint8)
+
+        output_path = output_root / phase_path / name
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        imageio.imwrite(output_path / 'x_est.png', np.fliplr(x_est))
