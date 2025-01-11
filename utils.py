eda9db4b82f97449df08addc15dcee856ee8aa1f
Martin Zach <martin.zach@epfl.ch>
2025-01-11 12:11:57 +0100

    unified led drawing code

--- utils.py
+++ utils.py
@@ -3,6 +3,44 @@ import torch as th
 import imageio.v3 as imageio
 import os
 from pathlib import Path
+import math
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+
+def draw_patterns(
+    positions: th.Tensor, na: float, indices: list[th.Tensor], root_path: Path
+):
+    for i, pat_indices in enumerate(indices):
+        fig, ax = plt.subplots(frameon="false", figsize=(5, 5))
+        fig.patch.set_facecolor("k")
+        r = math.sqrt(na**2 * positions[0, 2] ** 2 / (1 - na**2)) / 1000
+        circle = Circle((0, 0), r, facecolor="gray", edgecolor="none", linewidth=None)
+        ax.patch.set_facecolor("k")
+        ax.axis("equal")
+        ax.set_axis_off()
+        ax.add_patch(circle)
+        ax.scatter(
+            *positions[:, :2].T.cpu().numpy() / 1000,
+            facecolors="none",
+            edgecolors="darkgray",
+            s=100,
+            alpha=0,
+        )
+        for idx in pat_indices:
+            ax.scatter(
+                *positions[idx, :2].cpu().numpy() / 1000,
+                facecolors="w",
+                edgecolors="w",
+                s=100,
+            )
+
+        path = root_path / "patterns"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        fig.savefig(path / f"{i:02d}.pdf")
+        plt.close()
 
 
 def dump_experiments(x: th.Tensor, path: Path, crop: int):
@@ -40,14 +78,21 @@ def rmse(x, y):
 
 
 def dump_simulation(x, ref, path):
-    metrics = np.array([snr(th.angle(x), th.angle(ref)).cpu().numpy(), rmse(th.angle(x), th.angle(ref)).cpu().numpy()])[None]
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    metrics = np.array(
+        [
+            snr(th.angle(ref), th.angle(x)).cpu().numpy(),
+            rmse(th.angle(x), th.angle(ref)).cpu().numpy(),
+        ]
+    )[None]
     np.savetxt(path / "metrics.csv", metrics, delimiter=",", fmt="%.2f")
     ref_ft = th.fft.fft2(th.angle(ref))
     x_ft = th.fft.fft2(th.angle(x))
-    ft_error = th.fft.fftshift(th.abs(ref_ft - x_ft) / th.abs(ref_ft)).clamp_max(1)
-
-    if not os.path.exists(path):
-        os.makedirs(path)
+    ft_error = th.fft.fftshift(th.abs(ref_ft - x_ft) / th.abs(ref_ft + 1e-6)).clamp_max(
+        1
+    )
 
     x = th.angle(x)
     span = np.array([x.min().item().real, x.max().item().real])[None]
